417c47a9338c74302090f6a0e21ca6156c1aee0e
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-09-05 15:07:30 -0700

    uplaoded file and read it

--- face-recog.py
+++ face-recog.py
@@ -4,3 +4,22 @@ algorimith = '/Users/bhargavjakkaraju/Face-Scanner/haarcascade_frontalface_defau
 
 haar_cascade = cv2.CascadeClassifier(algorimith)
 
+file_name = '/Users/bhargavjakkaraju/Face-Scanner/face-image.jpeg'
+img = cv2.imread(file_name, 0)
+gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+#detect face
+faces = haar_cascade.detectMultiScale (
+    gray_img, scaleFactor = 1.05, minNeighbor = 5, minSize=(100,100)
+)
+
+i = 0
+for x, y, w, h in faces:
+    cropped_image = img[y: y + h, x: x + w]
+    target_file_name = 'stored-faces/' + str(i) + '.jpg'
+    cv2.imwrite(
+        target_file_name,
+        cropped_image,
+    )
+    i = i + 2
+ 
